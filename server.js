bbb2640e2ff80106261e652ca3474edbfd3fa02c
andybendavid <42923562+andybendavid@users.noreply.github.com>
2018-12-03 21:47:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ app.get('/showdetails', function(req,res) {
 				break;
 			}
 		}
-		if (((!items[i].photo) || (items[i].photo_mimetype == "application/pdf")) && ((!items[i].gps1) || (!items[i].gps2))) {
+		if ((!items[i].photo) || (items[i].photo_mimetype == "application/pdf")) && ((!items[i].gps1) || (!items[i].gps2)) {
 			db.collection("grades").find({r_id: req.query.id}).toArray(function(err,rnames){
 					res.render('detailsnophotonmap', {r: items[i], g: rnames});
 		
